ad8558838bc16b462cc4f84ceeadc4602fbd4ccc
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-08-16 23:30:34 -0400

    Creates gameboard when start button is clicked

--- index.js
+++ index.js
@@ -1,21 +1,53 @@
-function Gameboard(){
-    let gameboard = ['','','','','','','','',''];
+const Gameboard = (()=> {
+    let board = ["","","","","","","","",""]
 
     const render = () => {
         let boardHTML = "";
-        gameboard.forEach((square, index) => {
-            boardHTML += `<div class='square' id='square-${index}>${square}</div>`
-        })
-    };
 
-    document.querySelector('#board').innerHTML = boardHTML;
+        board.forEach((square, index) => {
+            boardHTML += `<div class='square' id='square-${index}'>${square}</div>`;
+                });
+
+        document.querySelector('#board').innerHTML = boardHTML;
+    };
 
     return{
         render,
     };
+})();
+
+const createPlayer = (name, mark) => {
+    return{
+        name,
+        mark
+    }
 };
+
+
+const Controller = (()=> {
+    let players = [];
+    let currentPlayerIndex;
+    let gameOver;
+
+    const start = () => {
+        players = [
+            (createPlayer(document.querySelector('#p1').value), 'X'),
+            (createPlayer(document.querySelector('#p2').value), 'O'),
+        ];
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+    };
+
+    return{
+        start
+    }
+})();
 
 const startButton = document.querySelector('#start-button');
 startButton.addEventListener('click', () => {
-    //Game.start();
+    Controller.start();
+    
 });
+
+Gameboard.render();
