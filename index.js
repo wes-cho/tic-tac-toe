300c667ae46166433d48684335e2252d656b100f
Wesley Cho <57739900+wes-cho@users.noreply.github.com>
2024-08-22 14:37:40 -0400

    Game recognizes a tie situation

--- index.js
+++ index.js
@@ -73,18 +73,39 @@ const Controller = (()=> {
         } else{
             return;
         }
+
+        if(checkWin(Gameboard.getBoard(), players[currentPlayerIndex].mark)){
+            gameOver = true;
+            alert(`${players[currentPlayerIndex].name} won!`);
+        } else if (checkTie(Gameboard.getBoard())){
+            gameOver = true;
+            alert("It's a tie!")
+        }
+
     };
 
-    const checkWin = () => {
-        //0-1-2
-        //3-4-5
-        //6-7-8
-        //0-3-6
-        //1-4-7
-        //2-5-8
-        //0-4-8
-        //2-4-6
+    function checkWin(board){
+        const winConditions = [
+            [0,1,2],
+            [3,4,5],
+            [6,7,8],
+            [0,3,6],
+            [1,4,7],
+            [2,5,8],
+            [0,4,8],
+            [2,4,6]
+        ];
+        for (let i=0; i<winConditions.length; i++){
+            const [a,b,c] = winConditions[i]
+            if (board[a] != ""  && board[a] === board[b] && board[a] === board[c]){
+                return true;
+            } 
+        }
+        return false;
+    };
 
+    function checkTie(board){
+        return board.every(cell => cell !== "");
     }
 
     return{
